dc645d25d7b5d1fd863dfa56c69829779ef09916
vjmenon <vj.menon93@gmail.com>
2014-10-15 15:17:44 -0400

    added random_junta

---
+++ random_juntas.m
@@ -0,0 +1,18 @@
+function [f, F] = random_juntas(n, K)
+      %n is the number of agents.
+      %K - vector which contains the indices of the agents(bits) who are part of the junta.  |k| = 1 => dictator.
+      
+      %Generate a random junta function (jF). i.e. it randomly assign 0/1 considering only the relevant bits
+      j = length(K);
+      
+      for y=0:2^j - 1
+          jF(y + 1) = randi(2) - 1;     %radom assignment
+      end      
+      
+      for x=0:2^n - 1
+        f(x + 1) =  jF(bi2de(bitget(x, K)) + 1);   %look at the relevant bits alone and see the corresponding jF function value
+      end      
+           
+      F = bool_ft(f);
+ 
+ end      
