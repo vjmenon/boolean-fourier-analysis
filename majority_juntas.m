32d6185aa5107279f72e90136596c131a50a108f
vjmenon <vj.menon93@gmail.com>
2014-10-15 15:27:59 -0400

    edited all files

--- majority_juntas.m
+++ majority_juntas.m
@@ -5,10 +5,10 @@ function [f, F] = majority_juntas(n, K)
       for x=0:2^n - 1     
           j = 0;
           for i = 1:length(K)
-              j = j + bitget(x, K(i)); %checking if the all the k-junta bits are 1; complete consensus junta
+              j = j + bitget(x, K(i));
           end              
           
-          if j >= length(K)/2
+          if j >= length(K)/2 %checking for majority
               f(x + 1) =  1;
           else
               f(x + 1) =  0;
--- random_juntas.m
+++ random_juntas.m
@@ -2,11 +2,11 @@ function [f, F] = random_juntas(n, K)
       %n is the number of agents.
       %K - vector which contains the indices of the agents(bits) who are part of the junta.  |k| = 1 => dictator.
       
-      %Generate a random junta function (jF). i.e. it randomly assign 0/1 considering only the relevant bits
+      %Generate a random junta function (jF). i.e. it randomly assigns 0/1 considering only the relevant bits
       j = length(K);
       
       for y=0:2^j - 1
-          jF(y + 1) = randi(2) - 1;     %radom assignment
+          jF(y + 1) = randi(2) - 1;     %random assignment
       end      
       
       for x=0:2^n - 1
